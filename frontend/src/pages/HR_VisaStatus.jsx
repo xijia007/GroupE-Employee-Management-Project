a34624d92c5b57b588944ebb2e4c1f943a41931e
Zhenjia Li <zhenjia@usc.edu>
2026-02-04 10:15:35 -0800

    Update HR_VisaStatus.jsx

--- frontend/src/pages/HR_VisaStatus.jsx
+++ frontend/src/pages/HR_VisaStatus.jsx
@@ -2,4 +2,5 @@ import HrVisaStatusManagement from "../components/hr_side/hr_visaStatusMangement
 
 export default function HR_VisaStatus() {
   return <HrVisaStatusManagement />;
+  ///re merge on these files
 }
